e337d79fc14372f27acef368de812c5e8356042d
gregapompe <grega.pompe@informa.si>
2015-02-17 13:37:55 +0100

    Bulk menu

--- API.pm
+++ API.pm
@@ -3,6 +3,17 @@ use Dancer ':syntax';
 use POSIX;
 use Dancer::Plugin::DBIC qw(schema resultset rset);
 
+sub schema_info {return TableEdit::Routes::API->schema_info;}
+
+sub menu {
+	my ($self, $menu) = @_;
+	$menu->{'Bulk tools'} = {sort => 120, items => [
+		{name => 'Product images', url => '#Product/images/bulkImageUpload'},
+		{name => 'Product navigation assign', url => '#Product/navigations/bulkAssign'},
+	]};
+	return $menu;
+}
+
 prefix '/';
 
 get '/' => sub {
