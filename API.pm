60b5d4586fecf23bc7620c36808fd5e60b7eb885
gregapompe <grega.pompe@informa.si>
2015-02-18 13:14:17 +0100

    Rename image file. Prepend manufacturer

--- API.pm
+++ API.pm
@@ -2,8 +2,10 @@ package TableEdit::Plugins::WestBranch::API;
 use Dancer ':syntax';
 use POSIX;
 use Dancer::Plugin::DBIC qw(schema resultset rset);
+use File::Copy;
 
 sub schema_info {return TableEdit::Routes::API->schema_info;}
+my $appdir = TableEdit::Config::appdir();
 
 sub menu {
 	my ($self, $menu) = @_;
@@ -29,5 +31,36 @@ get '/' => sub {
 prefix '/api';
 
 
+post '/bulkUploadImages/temp' => sub {
+	my $body = from_json request->body;
+
+	# Move product images to supplier dir
+	if ($body->{move}){
+		
+		my $class_info = schema_info->class($body->{class});
+		my $column_info = $class_info->column($body->{column});
+		my $media = $class_info->find_with_delimiter($body->{id});	
+		pass unless $media;
+		my $product = $media->products->first if $media->products;
+		pass unless $product;
+		my $manufacturer;
+		
+		my $nav = $product->navigations->first;
+		next unless defined $nav and $nav->type eq 'manufacturer';
+		$manufacturer = $nav->uri;
+		
+		my $path = $column_info->upload_dir;
+		my $dir = "$appdir/public/$path";
+		my $old_file = $media->file;
+		my $new_file = $manufacturer.'_'.$media->file;
+		$media->file($new_file);
+		move($dir.$old_file, $dir.$new_file);
+		$media->update;
+		
+		return 1;
+	}
+	
+	pass;
+};
 
 1;
